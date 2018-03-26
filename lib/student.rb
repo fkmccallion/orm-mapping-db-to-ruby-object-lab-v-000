69356526858aa774061646a9adf729b58f695eaf
Frank McCallion <fkmccallion@gmail.com>
2018-03-26 03:48:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,11 +76,22 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
-
+      WHERE grade = 10      
     SQL
 
     DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?     
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end.first
   end
