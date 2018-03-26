b40a9947058aba0e86dd209e73819327c05198ae
Frank McCallion <fkmccallion@gmail.com>
2018-03-26 03:37:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,11 +36,15 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
 
-  end
-
-  def self.size
-
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def save
